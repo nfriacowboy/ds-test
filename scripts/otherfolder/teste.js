f8eda2cdb6e8cfbe39e03b05008022b7eacaab8d
Nuno Lopes Fernandes <me@nfriacowboy.com>
2020-03-05 17:38:13 +0000

    test example

---
+++ scripts/otherfolder/teste.js
@@ -0,0 +1,9 @@
+let password = "this Is a password";
+let api_key = "this is an api key";
+
+let something = {
+  "password": "something here",
+  "api_key": "Y6RXN9H4YoZKRmRDV4Sg"
+};
+
+api_key = 'Y6RXN9H4YoZKRmRDV4Sg3';
